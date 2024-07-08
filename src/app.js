5063799a61365b4673cde93560ed2d9b61cb6598
Shekinahope <shekiesia0@gmail.com>
2024-07-09 00:20:32 +0300

    Added on JS
    Made changes on the <p>,arrays of the days, problem with the API key

--- src/app.js
+++ src/app.js
@@ -2,8 +2,40 @@ function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#weather-app-city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humid");
+  let windElement = document.querySelector("#speed");
+  let timeElement = document.querySelector("#time");
+
+  let date = new Date(response.data.time * 1000);
+
   cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
   temperatureElement.innerHTML = Math.round(temperature);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windElement.innerHTML = `${response.data.wind.speed}km/h`;
+}
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hours < 0) {
+    hours = `0${hours}`;
+  }
+  return `${day} ${hours}:${minutes},`;
 }
 
 function searchCity(city) {
@@ -17,7 +49,6 @@ function handleSearchSubmit(event) {
   event.preventDefault();
 
   let searchInput = document.querySelector("#search-form-input");
-  console.log(searchInput.value);
 
   searchCity(searchInput.value);
 }
